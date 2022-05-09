86eaa5b0a580955f63f726b3a36f18f18ddb2838
Salad24 <veggiemagic1124@gmail.com>
2022-05-09 14:06:08 -0400

    attempt #1 to fix display on mobile

--- JS/main.js
+++ JS/main.js
@@ -1702,6 +1702,8 @@ function shareLostResult() {
       }
     }
   }
+  string += "\n\nhttps://salad24.github.io/Cinele";
+  
   navigator.clipboard.writeText(string).then(function () {
     const shareTexts = document.querySelectorAll(".share-text");
     for (let i = 0; i < shareTexts.length; i++) {
